be7cfb41cbd0eb6a8afc9a4ef7a23aa95ecd2952
gftyfyjgj87 <bedoo4191@gmail.com>
2026-03-29 02:17:00 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    port: 5174,
+    strictPort: true,
+    hmr: {
+      overlay: false,
+    },
+  },
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  optimizeDeps: {
+    force: true, // ✅ سيب Vite يدير الدنيا
+  },
+}));
